01fd26f0222b0d94d7b6dab1d61183c4c018d409
Code-Toad29 <princetyrique52@gmail.com>
2024-10-07 11:22:23 +0200

    create and run server

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,73 @@
 
 
+// import React from 'react';
+// import { PiShoppingCartLight } from 'react-icons/pi';
+// import { MdMenu } from 'react-icons/md';
+// import { MdFavorite } from 'react-icons/md';
+// import { Link } from 'react-router-dom';
+
+// const Navbar = () => {
+//     return (
+//         <header className='bg-black'>
+//             <nav className='container mx-auto flex justify-between items-center py-6'>
+//                 {/* Logo section */}
+//                 <img src='/logo.jpg' alt='Company logo' width={80} />
+
+//                 <div className='text-xl flex items-center  relative gap-20 font-bold uppercase'>
+//                     {/* Menu section */}
+//                     <div > 
+//                         <ul className='flex gap-8 text-yellow-600' role="navigation">
+
+                            
+                            
+//                             <li><Link to='/home' className='hover:text-white'>Home</Link></li>
+//                             <li><Link to='/buy' className='hover:text-white'>Buy</Link></li>
+//                             <li><Link to='/rent' className='hover:text-white'>Rent</Link></li>
+//                             <li><Link to='/sell' className='hover:text-white'>Sell</Link></li>
+//                             <li><Link to='/contact' className='hover:text-white'>Contact</Link></li>
+//                             <li>
+//                             <a href=''className='text-yellow-600 hover:text-white transition duration-200'> 
+//                             <MdFavorite/>
+//                             </a>
+//                             </li>  
+//                             <li>
+//                             <a href='' className='text-yellow-600 hover:text-white transition duration-200'>
+//                                 <PiShoppingCartLight/> 
+//                             </a>   
+//                             </li>
+//                             <li>
+//                             <Link to='/sign-up' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Sign up</Link>
+//                             </li>
+//                             {/* <li>
+//                               <Link to='/log-in'  className  >className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Log in</li>
+//                             </Link>
+//                             <li> */}
+//                             <li>
+//                             <Link to='/log-in' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>
+//                              Log in
+//                              </Link>
+//                              </li>
+
+                    
+//                             <a href=''className='text-yellow-600 hover:text-white transition duration-200'> 
+//                             <MdMenu/> 
+//                             </a>
+//                             </li>
+        
+
+//                         </ul>   
+//                     </div> 
+//                 </div>
+//             </nav>
+//         </header>
+//     );
+// }
+
+// export default Navbar;
 import React from 'react';
 import { PiShoppingCartLight } from 'react-icons/pi';
-import { MdMenu } from 'react-icons/md';
-import { MdFavorite } from 'react-icons/md';
+import { MdMenu, MdFavorite } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -12,43 +76,36 @@ const Navbar = () => {
                 {/* Logo section */}
                 <img src='/logo.jpg' alt='Company logo' width={80} />
 
-                <div className='text-xl flex items-center  relative gap-20 font-bold uppercase'>
+                <div className='text-xl flex items-center relative gap-20 font-bold uppercase'>
                     {/* Menu section */}
-                    <div > 
-                        <ul className='flex gap-8 text-yellow-600' role="navigation">
-
-                            
-                            <li><a href='/home' className='hover:text-white'>Home</a></li>
-                            <li><a href='/buy' className='hover:text-white'>Buy</a></li>
-                            <li><a href='/rent' className='hover:text-white'>Rent</a></li>
-                            <li><a href='/sell' className='hover:text-white'>Sell</a></li>
-                            <li><a href='/contact' className='hover:text-white'>Contact</a></li>
-                            <li>
-                            <a href=''className='text-yellow-600 hover:text-white transition duration-200'> 
-                            <MdFavorite/>
-                            </a>
-                            </li>  
-                            <li>
-                            <a href='' className='text-yellow-600 hover:text-white transition duration-200'>
-                                <PiShoppingCartLight/> 
-                            </a>   
-                            </li>
-                            <li>
-                            <a href='' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Sign up</a>
-                            </li>
-                            <li>
-                                <a href='' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Log in</a>
-                            </li>
-                            <li>
-                    
-                            <a href=''className='text-yellow-600 hover:text-white transition duration-200'> 
-                            <MdMenu/> 
-                            </a>
-                            </li>
-        
-
-                        </ul>   
-                    </div> 
+                    <ul className='flex gap-8 text-yellow-600' role="navigation">
+                        <li><Link to='/home' className='hover:text-white'>Home</Link></li>
+                        <li><Link to='/buy' className='hover:text-white'>Buy</Link></li>
+                        <li><Link to='/rent' className='hover:text-white'>Rent</Link></li>
+                        <li><Link to='/sell' className='hover:text-white'>Sell</Link></li>
+                        <li><Link to='/contact' className='hover:text-white'>Contact</Link></li>
+                        <li>
+                            <Link to='/favorites' className='text-yellow-600 hover:text-white transition duration-200'>
+                                <MdFavorite />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/cart' className='text-yellow-600 hover:text-white transition duration-200'>
+                                <PiShoppingCartLight />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/sign-up' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Sign up</Link>
+                        </li>
+                        <li>
+                            <Link to='/log-in' className='bg-yellow-600 text-white px-4 py-2 rounded-full hover:bg-yellow-500 transition duration-200'>Log in</Link>
+                        </li>
+                        <li>
+                            <Link to='/menu' className='text-yellow-600 hover:text-white transition duration-200'>
+                                <MdMenu />
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
