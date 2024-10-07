d538911edda5913648d4400e19f2ea6fe5d7d6fc
Code-Toad29 <princetyrique52@gmail.com>
2024-10-07 08:47:05 +0200

    second save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import{BrowserRouter,Routes,Route} from 'react-router-dom';
-import React from 'react'
-import Home from './pages/Home';
-import Buy from './pages/Buy';
-import Rent from './pages/Rent';
-import Sell from './pages/Sell';
-import Contact from './pages/Contact';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Hero  from  './components/Hero';
-import Navbar from './components/Navbar';
-import Companies from './components/Companies';
+// import{BrowserRouter,Routes,Route} from 'react-router-dom';
+// import React from 'react'
+// import Home from './pages/Home';
+// import Buy from './pages/Buy';
+// import Rent from './pages/Rent';
+// import Sell from './pages/Sell';
+// import Contact from './pages/Contact';
+// import Profile from './pages/Profile';
+// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+// import Hero  from  './components/Hero';
+// import Navbar from './components/Navbar';
+// import Companies from './components/Companies';
 
 // function App() {
 // return( <div className='overflow-x-hidden'>
@@ -22,9 +22,45 @@ import Companies from './components/Companies';
 //       </div>);    
 // };
 // export default App;
-export default function App(){
-      return<BrowserRouter>
-      <Navbar/>
+// export default function App(){
+//       return<BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/buy' element={<Buy />} />
+//         <Route path='/rent' element={<Rent />} />
+//         <Route path='/sell' element={<Sell />} />
+//         <Route path='/contact' element={<Contact />} />
+//         <Route path='/profile' element={<Profile />} />
+//         <Route path='/sign-in' element={<SignIn />} />
+//         <Route path='/sign-up' element={<SignUp />} />
+
+
+//       </Routes>
+
+
+//       </BrowserRouter>
+// }
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Buy from './pages/Buy';
+import Rent from './pages/Rent';
+import Sell from './pages/Sell';
+import Contact from './pages/Contact';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Companies from './components/Companies';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Hero />
+      <Companies />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/buy' element={<Buy />} />
@@ -34,12 +70,9 @@ export default function App(){
         <Route path='/profile' element={<Profile />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-
-
       </Routes>
-
-
-      </BrowserRouter>
+    </BrowserRouter>
+  );
 }
 
 
